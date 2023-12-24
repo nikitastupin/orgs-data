25db87d53254513273f2f2c87f75cbf2ac235082
Nikita Stupin <18281368+nikitastupin@users.noreply.github.com>
2023-12-24 17:02:35 +0100

    swap lines

--- scripts/all-orgs.sh
+++ scripts/all-orgs.sh
@@ -8,5 +8,5 @@ find "$ROOT/orgs-data" -type f      \
   | xargs cat                       \
   | cut -f 2                        \
   | grep -E '^https://github\.com/' \
-  | sort -u                         \
-  | cut -d / -f 4
+  | cut -d / -f 4                   \
+  | sort -u
